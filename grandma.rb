dfcb9c98d29ae0fdcd2de3bb7cba024d19ffca13
corytveten <corytveten@gmail.com>
2018-04-20 17:11:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
 # unless you shout it (type in all capitals).
   if
     phrase == "I LOVE YOU GRANDMAN!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif
     phrase == upcase
     return "NO, NOT SINCE 1938"
