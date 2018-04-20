d30b80bc18021f425ab406dceca3333bdb7296a0
corytveten <corytveten@gmail.com>
2018-04-20 17:14:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     phrase == "I LOVE YOU GRANDMAN!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif
-    phrase == upcase
+    phrase == phrase.upcase
     return "NO, NOT SINCE 1938"
   else
     return "SPEAK UP, SONNY!"
